07b0f721bfd5e036e559a70e172e7a688344c0f1
Rodrigo Muniz <rodrigo@mamangava.com>
2018-02-16 18:01:26 -0200

    Chat App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCqtSRgrn4iXfrVy2bAWdGTQdWgph6_jOk",
+  authDomain: "chatapp-a039b.firebaseapp.com",
+  databaseURL: "https://chatapp-a039b.firebaseio.com",
+  projectId: "chatapp-a039b",
+  storageBucket: "",
+  messagingSenderId: "995780415093"
+};
+
 
 
 @NgModule({
@@ -10,7 +24,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
